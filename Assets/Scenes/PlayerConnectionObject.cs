96069af0bed57f0b791090859bd3acd03b4ecf54
deruitda <deruitda@mail.gvsu.edu>
2019-10-11 17:18:00 -0400

    initial checkin

---
+++ Assets/Scenes/PlayerConnectionObject.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class PlayerConnectionObject : NetworkBehaviour
+{
+    public GameObject PlayerUnitPrefab;
+    [SyncVar] //if a sync var is changed on the SERVER, then all clients will be updated with the new value
+    public string PlayerName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isLocalPlayer)
+            CmdSpawnUnit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            PlayerName = "TEST" + Random.Range(0, 100).ToString();
+            Debug.Log("Send server new name");
+            CmdChangePlayerName(PlayerName);
+        }
+    }
+
+    [Command]
+    void CmdSpawnUnit()
+    {
+        GameObject gameObject = Instantiate(PlayerUnitPrefab);
+
+        NetworkServer.SpawnWithClientAuthority(gameObject, connectionToClient);
+    }
+
+    [Command]
+    void CmdChangePlayerName(string name)
+    {
+        Debug.Log($"Player name: {name}");
+        PlayerName = name;
+    }
+}
