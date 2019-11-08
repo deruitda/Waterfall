46841e1a2934a3d0eb270654bb1bae8572f92bdd
deruitda <deruitda@mail.gvsu.edu>
2019-11-07 21:23:28 -0500

    Added turn number to player, increment turn number when player is added

--- Assets/CustomNetworkManager.cs
+++ Assets/CustomNetworkManager.cs
@@ -5,9 +5,13 @@ using UnityEngine.Networking;
 
 public class CustomNetworkManager : NetworkManager
 {
+    public int PlayerCount { get; private set; } = 0;
+
     public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
     {
         var player = (GameObject)GameObject.Instantiate(playerPrefab, new Vector3(9.65f, 3.36f, -14.28f), Quaternion.identity);
-        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+        player.GetComponent<PlayerInfo>().TurnNumber = PlayerCount++;
+
+        NetworkServer.AddPlayerForConnection(conn, player, (short)PlayerCount);
     }
 }
