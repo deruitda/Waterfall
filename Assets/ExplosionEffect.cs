10f2d73b50a43d96887bf2b522ebeaea5ed22986
deruitda <deruitda@mail.gvsu.edu>
2019-11-08 14:58:27 -0500

    Explosion command has local authority so client can call command

--- Assets/ExplosionEffect.cs
+++ Assets/ExplosionEffect.cs
@@ -1,12 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class ExplosionEffect : MonoBehaviour
+public class ExplosionEffect : NetworkBehaviour
 {
     public GameObject _explisionPrefab;
 
     public void Explode()
+    {
+        CmdExplode();
+    }
+
+    [Command]
+    void CmdExplode()
+    {
+        RpcExplode();
+    }
+
+    [ClientRpc]
+    private void RpcExplode()
     {
         Instantiate(_explisionPrefab, this.transform.position, this.transform.rotation);
     }
