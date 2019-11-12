b6272da422863c6343d220eaad0d6deebd7ef5aa
deruitda <deruitda@mail.gvsu.edu>
2019-11-11 20:13:28 -0500

    kind of have can explosion sound

--- Assets/ExplosionEffect.cs
+++ Assets/ExplosionEffect.cs
@@ -7,6 +7,7 @@ using UnityEngine.Networking;
 public class ExplosionEffect : NetworkBehaviour
 {
     public GameObject _explisionPrefab;
+    public AudioSource _explosionAudio;
 
     public void Explode()
     {
@@ -22,6 +23,10 @@ public class ExplosionEffect : NetworkBehaviour
     [ClientRpc]
     private void RpcExplode()
     {
+        _explosionAudio.volume = 1.0f; //for some reason sound only plays when the "Play on Awake" box is ticked, so I default the sound to 0 so it doesn't play on awake, and then set it back to 100
+        _explosionAudio.enabled = true;
+        _explosionAudio.Play();
         Instantiate(_explisionPrefab, this.transform.position, this.transform.rotation);
+        _explosionAudio.Stop();
     }
 }
