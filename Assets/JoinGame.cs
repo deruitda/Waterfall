22e20246e2f4cd449835935c06988b9e532f227d
deruitda <deruitda@mail.gvsu.edu>
2019-11-24 18:06:31 -0500

    join game works (with unfinished UI)

---
+++ Assets/JoinGame.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using UnityEngine.UI;
+
+public class JoinGame : MonoBehaviour
+{
+    private NetworkManager _networkManager;
+    private List<GameObject> _roomList;
+    [SerializeField]
+    private Text _status;
+    [SerializeField]
+    private GameObject _roomListItemPrefab;
+    [SerializeField]
+    private Transform _roomListParent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _roomList = new List<GameObject>();
+        _networkManager = NetworkManager.singleton;
+
+        if (_networkManager.matchMaker == null)
+            _networkManager.StartMatchMaker();
+
+        RefreshRoomList();
+    }
+
+    public void RefreshRoomList()
+    {
+        ClearRoomList();
+        _networkManager.matchMaker.ListMatches(0, 20, "", false, 0, 0, OnMatchList);
+        _status.text = "Loading matches...";
+    }
+
+    public void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matches)
+    {
+        _status.text = "";
+
+        if (matches == null)
+        {
+            _status.text = "Could not load rooms!!!!!!!!!!";
+            return;
+        }
+        else if(matches.Count == 0)
+        {
+            _status.text = "No active rooms available";
+            return;
+        }
+
+        ClearRoomList();
+
+        foreach(MatchInfoSnapshot match in matches)
+        {
+            //add the room button object and parent it to the scrollview
+            GameObject roomItemGameObject = Instantiate(_roomListItemPrefab);
+            roomItemGameObject.transform.SetParent(_roomListParent);
+
+            RoomListItem roomListItem = roomItemGameObject.GetComponent<RoomListItem>();
+            if(roomListItem != null)
+            {
+                roomListItem.Setup(match, JoinRoom);
+            }
+
+            _roomList.Add(roomItemGameObject);
+        }
+    }
+
+    private void ClearRoomList()
+    {
+        for (int i = 0; i < _roomList.Count; i++)
+        {
+            Destroy(_roomList[i]);
+        }
+
+        _roomList.Clear();
+    }
+
+    public void JoinRoom(MatchInfoSnapshot match)
+    {
+        _status.text = $"Joining match {match.name}";
+        Debug.Log($"Joining match: {match.name}");
+        _networkManager.matchMaker.JoinMatch(match.networkId, "", "", "", 0, 0, _networkManager.OnMatchJoined);
+    }
+}
---
+++ Assets/RoomListItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking.Match;
+using UnityEngine.UI;
+
+public class RoomListItem : MonoBehaviour
+{
+    [SerializeField]
+    private Text _roomNameText;
+    public delegate void JoinRoomDelegate(MatchInfoSnapshot match);
+    public JoinRoomDelegate _joinRoomCallback;
+
+    private MatchInfoSnapshot _match;
+
+    public void Setup(MatchInfoSnapshot match, JoinRoomDelegate callback)
+    {
+        _match = match;
+        _joinRoomCallback = callback;
+        _roomNameText.text = $"{match.name} ({match.currentSize}/{match.maxSize})";
+    }
+
+    public void JoinGame()
+    {
+        _joinRoomCallback.Invoke(_match);
+    }
+}
