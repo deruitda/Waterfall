a63ee932f5dc1cb1d468744ece5c133291f6ee48
deruitda <deruitda@mail.gvsu.edu>
2019-11-24 16:40:01 -0500

    Host game UI working

---
+++ Assets/Scripts/HostGame.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostGame : MonoBehaviour
+{
+    [SerializeField]
+    private uint _roomSize = 8;
+    private string _roomName;
+    private NetworkManager _networkManager;
+
+    public void Start()
+    {
+        _networkManager = NetworkManager.singleton;
+
+        if (_networkManager.matchMaker == null)
+            _networkManager.StartMatchMaker();
+
+
+    }
+
+    public void SetRoomName(string roomName)
+    {
+        _roomName = roomName;
+    }
+
+    public void CreateRoom()
+    {
+        if (!string.IsNullOrEmpty(_roomName))
+        {
+            Debug.Log($"Creating room {_roomName}...");
+
+            _networkManager.matchMaker.CreateMatch(_roomName, _roomSize, true, "", "", "", 0, 0, _networkManager.OnMatchCreate);
+        }
+    }
+}
