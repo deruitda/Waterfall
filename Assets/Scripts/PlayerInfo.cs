2dd489aac06dcdaa5ce63dec29b30d35f0e45786
nickmws <nick@marblewebsolutions.com>
2019-11-26 22:26:02 -0500

    Player Name 80% Done

--- Assets/Scripts/PlayerInfo.cs
+++ Assets/Scripts/PlayerInfo.cs
@@ -11,13 +11,27 @@ public class PlayerInfo : NetworkBehaviour
     [SyncVar]
     public string Name;
     public Text DisplayText;
-
+    public InputField _yourNameInput;
+    public GameObject _playerNameCanvas;
 
     public void Update()
     {
         if (!DisplayText.text.Equals(Name))
         {
             DisplayText.text = Name;
+        }
+    }
+
+    public void SetName()
+    {
+        _yourNameInput = GameObject.Find("YourNameInput").GetComponent<InputField>();
+        _playerNameCanvas = GameObject.Find("YourNameCanvas");
+
+        if (!string.IsNullOrEmpty(_yourNameInput.text))
+        {
+            Name = _yourNameInput.text;
+            _playerNameCanvas.SetActive(false);
+            _yourNameInput.text = "";
         }
     }
 }
